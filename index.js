8dc97c5603ee58cb7254dc5f7c44e248dc3cdfb8
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-07-22 15:43:15 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-let test = () => {
-    console.log('testttt')
+
+let conversion = () => {
+    // GET VALUES FROM DOM
+    let usersName = document.querySelector('.name-input').value
+    let usersEmail = document.querySelector('.email-input').value
+    // CHECK IF FIELDS ARE FILLED OUT
+    if (usersName.indexOf(' ') !== -1 && usersEmail) {
+        // SPLIT FULL NAME INTO FIRST AND LAST
+        let nameArr = usersName.split(' ')
+        let firstName = nameArr[0]
+        let lastName = nameArr[1]
+        // PUSH NEW DOCUMENT TO FIREBASE
+        firebase.firestore().collection("youtube-leads").add({
+            firstName: firstName,
+            lastName: lastName,
+            email: usersEmail,
+            date: Date()
+        }).then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
+        // CONSOLE LOG COLLECTED INFORMATION
+        console.log(`First Name: ${firstName}`)
+        console.log(`Last Name: ${lastName}`)
+        console.log(`Email: ${usersEmail}`)
+
+        // TRACK CONVERSION
+        gtag_report_conversion()
+    } else if (usersName.indexOf(' ') === -1) {
+        // IF NAME IS NOT FULL
+        alert('Please enter your full name')
+    }
 }
 
-document.querySelector('button').addEventListener('click', test)
+document.querySelector('.conversion-btn').addEventListener('click', conversion)
