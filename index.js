4cde4f15622d3f30273f47f85bcd1182ace828bb
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-07-28 10:06:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 document.querySelector('video').click()
+let hasPlayed = false;
+
+let play = () => {
+    if (hasPlayed === false) {
+        document.querySelector('video').play()
+        console.log('dsfs')
+    }
+    hasPlayed = true
+}
+
+document.addEventListener('mousemove', play)
+
 
 let conversion = () => {
     // GET VALUES FROM DOM
